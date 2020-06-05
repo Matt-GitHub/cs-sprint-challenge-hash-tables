35d24999e7cf4e014bc26ccdf796b3a98abcb57d
Matt-GitHub <matthewjamesbedard@outlook.com>
2020-06-05 13:57:14 -0400

    mvp+stretch

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,31 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # weights is equal to an array =>  weights = [12, 18, 14, 4, 7, 9, 21]
+    # limit is the desired comibined weight from a pair in weights
+    checker = {}
 
+    for i in range(len(weights)):
+        if weights[i] not in checker:
+            checker[weights[i]] = i
+
+    for i in range(len(weights)):
+        target = limit - weights[i]
+        if target in checker:
+            if checker[target] == i:
+                pass
+                # return [i + 1, checker[target]]
+            elif checker[target] > i:
+                return [checker[target], i]
+            else:
+                return [i, checker[target]]
     return None
+
+
+# weights_4 = [12, 6, 7, 14, 19, 3, 0, 25, 40]
+
+# print(get_indices_of_item_weights(weights_4, len(weights_4), 22))
+
+# weights_4 = [12, 6, 7, 14, 19, 3, 0, 25, 40]
+# print(get_indices_of_item_weights(weights_4, 9, 7))
+
+# weights_2 = [1, 2, 3, 3, 4, 2, 4]
+# print(get_indices_of_item_weights(weights_2, 2, 8))
--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,14 +1,27 @@
 # Your code here
 
 
-
 def finder(files, queries):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    cache = {}
+    for file in files:
+        # print(file.split('/'))
+        split = file.split('/')
+        for x in split:
+            if x in cache:
+                cache[x] = [cache[x], file]
+            else:
+                cache[x] = file
+    solution = []
+    for value in queries:
+        if value in cache:
+            solution.append(cache[value])
+    answer = []
 
-    return result
+    def final(arr):
+        for i in arr:
+            final(i) if isinstance(i, list) else answer.append(i)
+    final(solution)
+    return answer
 
 
 if __name__ == "__main__":
@@ -20,6 +33,7 @@ if __name__ == "__main__":
     queries = [
         "foo",
         "qux",
-        "baz"
+        "baz",
+        "bin"
     ]
     print(finder(files, queries))
